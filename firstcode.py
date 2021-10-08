923c1af0a22c92b881fccba26d9ca95e827ad39e
satish <2101398@sit.singaporetech.edu.sg>
2021-10-08 09:17:09 +0800

    added book lib

--- firstcode.py
+++ firstcode.py
@@ -1,6 +1,8 @@
 import sys
 import time
+import lib_book as lb
 
 print("good day and hello satish")
+lb.types("electronics")
 
 
---
+++ lib_book.py
@@ -0,0 +1,5 @@
+import sys
+
+def types (x):
+	print("my book is: ", x)
+
